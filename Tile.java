187ca4edfab88793aab97ded2159f97e3d56f53c
Laura Goon <goon.laura@gmail.com>
2022-11-21 18:41:10 -0500

    Update and rename tiles/Tile.java to Tile.java

---
+++ Tile.java
@@ -0,0 +1,10 @@
+public class Tile {
+    private TileSet set;
+    private TileValue value;
+
+    public void setSet(TileSet set) { this.set = set; }
+    public TileSet getSet() { return set; }
+    
+    public void setValue(TileValue value) { this.value = value; }
+    public TileValue getValue() { return value; }
+}
--- tiles/Tile.java
+++
@@ -1,6 +0,0 @@
-public class Tile {
-    private TileSet set;
-
-    public void setSet(TileSet set) { this.set = set; }
-    public TileSet getSet() { return set; }
-}
