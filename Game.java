5db52575ad108747414f7e41edae842782505012
Laura Goon <goon.laura@gmail.com>
2022-11-21 20:00:07 -0500

    Create Game.java

---
+++ Game.java
@@ -0,0 +1,25 @@
+import Tile
+
+public class Game
+{
+    public ArrayList<Tile> tiles;
+  
+    public Game()
+    {
+        tiles = new ArrayList<Tile>();
+        init();
+    }
+  
+    private void genTiles()
+    {
+        for (int i = 0; i < 9; i++)
+        {
+            // TODO:
+        }
+    }
+  
+    private void init()
+    {
+        genTiles();
+    }
+}
