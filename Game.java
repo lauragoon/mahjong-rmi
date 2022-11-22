147fbcbbd4b32e7232f2f8df2667df6b818fab3b
Laura Goon <goon.laura@gmail.com>
2022-11-21 22:29:07 -0500

    add checks for partial winning states

--- Game.java
+++ Game.java
@@ -2,6 +2,7 @@ public class Game
 {
     public ArrayList<Tile> tiles;
     public HashMap<Integer, TileSet> numValueMap;
+    public HashMap<TileSet, Integer> valueNumMap; // quick, hacky way to do bi-directional map for now
   
     public Game()
     {
@@ -15,7 +16,18 @@ public class Game
             entry(6, TileValue.SIX),
             entry(7, TileValue.SEVEN),
             entry(8, TileValue.EIGHT),
-            entry(9, TileValue.NINE)     
+            entry(9, TileValue.NINE),
+        );
+        valueNumMap = Map.ofEntries(
+            entry(TileValue.ONE, 1),
+            entry(TileValue.TWO, 2),
+            entry(TileValue.THREE, 3),
+            entry(TileValue.FOUR, 4),
+            entry(TileValue.FIVE, 5),
+            entry(TileValue.SIX, 6),
+            entry(TileValue.SEVEN, 7),
+            entry(TileValue.EIGHT, 8),
+            entry(TileValue.NINE, 9)
         );
         
         init();
@@ -62,5 +74,65 @@ public class Game
     private void init()
     {
         genTiles();
+    }
+    
+    private Boolean isTripleSequence(List<Tile> tiles)
+    {
+        // check if size is 3
+        if (tiles.size() != 3) { return False; }
+        
+        // check it is same suit
+        for (int i = 0; i < tiles.size()-1; i++)
+        {
+            if (tiles.get(i).getSet() != tiles.get(i+1).getSet()) { return False; }
+        }
+        
+        // check that it is suit that is sequence-able
+        TileSet thisSet = tiles.get(0).getSet();
+        if (thisSet != TileSet.TEN_THOUSAND || thisSet != TileSet.CIRCLE || thisSet != TileSet.STICKS) { return False; }
+        
+        // verify sequence
+        Collections.sort(tiles);
+        for (int i = 0; i < tiles.size()-1, i++)
+        {
+            if (valueNumMap.get(tiles.get(i)) != valueNumMap.get(tiles.get(i+1)) - 1) { return False; }
+        }
+        
+        return True;
+    }
+    
+    private Boolean isThreeOfAKind(List<Tile> tiles)
+    {
+        // check if size is 3
+        if (tiles.size() != 3) { return False; }
+        
+        // check if same suit and value
+        for (int i = 0; i < tiles.size()-1; i++)
+        {
+            if (tiles.get(i).getSet() != tiles.get(i+1).getSet()) { return False; }
+            if (tiles.get(i).getValue() != tiles.get(i+1).getValue()) { return False; }
+        }
+        
+        // check that it is three-of-a-kind-able
+        TileSet thisSet = tiles.get(0).getSet();
+        if (thisSet != TileSet.TEN_THOUSAND || thisSet != TileSet.CIRCLE || thisSet != TileSet.STICKS || thisSet != TileSet.WINDS || thisSet != TileSet.DRAGONS) { return False; }
+        
+        return True;
+    }
+    
+    private Boolean isPair(List<Tile> tiles)
+    {
+        // check if size is 2
+        if (tiles.size() != 2) { return False; }
+        
+        // check if same suit and value
+        if (tiles.get(0).getSet() != tiles.get(1).getSet()) { return False; }
+        if (tiles.get(0).getValue() != tiles.get(1).getValue()) { return False; }
+        
+        // check that it is pair-able
+        TileSet thisSet = tiles.get(0).getSet();
+        if (thisSet != TileSet.TEN_THOUSAND || thisSet != TileSet.CIRCLE || thisSet != TileSet.STICKS || thisSet != TileSet.WINDS || thisSet != TileSet.DRAGONS) { return False; }
+        
+        return True;
     }
 }
